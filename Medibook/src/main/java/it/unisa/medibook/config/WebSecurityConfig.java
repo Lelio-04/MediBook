bcd9948f36665134d750923b4f2122cc5ce46d7a
Lelio-04 <crisci.lelio04@gmail.com>
2025-12-31 14:38:16 +0100

    WebSecurityConfigModifica

--- Medibook/src/main/java/it/unisa/medibook/config/WebSecurityConfig.java
+++ Medibook/src/main/java/it/unisa/medibook/config/WebSecurityConfig.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
+    //commento
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
