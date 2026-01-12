7f151c27d4740bee239d7b352d13a4643ed0c99f
Lelio-04 <crisci.lelio04@gmail.com>
2026-01-12 18:01:23 +0100

    fix integretion test

--- Medibook/src/test/java/it/unisa/medibook/service/GestioneUtenzaTest.java
+++ Medibook/src/test/java/it/unisa/medibook/service/GestioneUtenzaTest.java
@@ -14,7 +14,6 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -142,6 +141,7 @@ class GestioneUtenzaTest {
         assertEquals("PAZIENTE", nuovoP.getRuolo());
         assertEquals("hash_sicuro", nuovoP.getPassword());
     }
+
     @Test
     void cambioPasswordObbligatorio() {
         // --- TC_CPW_1: Cambio Password Corretto ---
